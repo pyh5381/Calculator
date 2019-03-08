bb0621f693d9f7b06f5f33a5da2e2cc9c0ad8ddb
DevPark <pyh5381@naver.com>
2019-03-08 23:17:54 +0900

    Button Click && KeyPress Event Add

--- Calculator/src/park/StandardPanel.java
+++ Calculator/src/park/StandardPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.DecimalFormat;
 import java.util.regex.Pattern;
 
 import javax.swing.JButton;
@@ -22,7 +23,7 @@ public class StandardPanel implements ActionListener, KeyListener {
 	private JPanel btnPane = new JPanel(new GridLayout(5, 4, 1, 1));
 	
 	private JButton[] btn = new JButton[20];
-	private String[] btnName = { "CE", "C", "☜", "/"
+	private String[] btnName = { "CE", "C", "←", "/"
             					 ,"7", "8", "9", "*"
             					 ,"4", "5", "6", "-"
             					 ,"1", "2", "3", "+"
@@ -38,31 +39,83 @@ public class StandardPanel implements ActionListener, KeyListener {
 		calculator.add(resultPane, BorderLayout.NORTH);
 		
 		/*---------- 버튼 출력을 위한 패널 설정 ----------*/
-		for (int i = 0; i < btnName.length; i++) {
+		for (int i = 0; i < btnName.length; i++) 
+		{
 			btn[i] = new JButton(btnName[i]);
 			btnPane.add(btn[i]);
 			btn[i].setFont(new Font("나눔", Font.BOLD, 25));
 			
+			// 숫자 버튼과 나머지 버튼에 다른색을 주기위한 조건식
 			if (Pattern.matches("[0-9]", btnName[i])) {
 				btn[i].setBackground(new Color(235, 235, 235));
 			}
 			else {
 				btn[i].setBackground(new Color(220, 220, 220));
 			}
+			// 자기자신이 Override한 버튼클릭 및 키보드입력 이벤트 관련 메소드로 Listener 추가
+			btn[i].addActionListener(this);
+			btn[i].addKeyListener(this);
 		}
 		
 		calculator.add(btnPane, BorderLayout.CENTER);
 	}
 	
+	public String digitNum(String tmpResult) {
+		DecimalFormat df = new DecimalFormat("#,###");
+		tmpResult = tmpResult.replaceAll(",", "");
+		
+		return df.format(Long.parseLong(tmpResult));
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		String clickBtn = e.getActionCommand();
+		String tmpResult = result.getText();
 		
+		/*--------- 숫자 버튼 클릭시 처리 이벤트 ---------*/
+		if (Pattern.matches("[0-9]", clickBtn)) {
+			if (tmpResult == "0") {
+				result.setText(clickBtn);
+			}
+			else {
+				tmpResult += clickBtn;
+				result.setText(digitNum(tmpResult));
+			}
+		}
+		
+		/*--------- 그외에 다른 버튼 클릭시 처리 이벤트 ---------*/
+		switch (clickBtn) {
+			case "CE":
+				result.setText("0");
+				break;
+			case "C":
+				result.setText("0");
+				break;
+			default:
+				break;
+		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		String pressBtn = KeyEvent.getKeyText(e.getKeyCode());
+		String tmpResult = result.getText();
+		
+		/*--------- 숫자 키보드 입력시 처리 이벤트 ---------*/
+		if (Pattern.matches("[0-9]", pressBtn)) {
+			if (tmpResult == "0") {
+				result.setText(pressBtn);
+			}
+			else {
+				tmpResult += pressBtn;
+				result.setText(digitNum(tmpResult));
+			}
+		}
+		
+		/*--------- 그외에 키보드 입력시 처리 이벤트 ---------*/
 		
 	}
+	
 	// 아래 2개 메소드는 사용하지 않음
 	@Override
 	public void keyTyped(KeyEvent e) { }
